80b4016fc631460ed49b4c7ace3be6723ec8fcd4
alexBaquerizo <elbatracio_21@hotmail.com>
2016-02-17 12:25:19 +0100

    avances sobre el controller respuesta

--- api/controllers/RespuestaController.js
+++ api/controllers/RespuestaController.js
@@ -7,22 +7,34 @@
 
 module.exports = {
 	respuestaAlumno: function(req, res, next) {
-		var miRespuesta = req.body.answered;
+		var Respuesta = req.body.answered;
+		var Puntos =0;
 
 		Opcion.find({
 			where: { pregunta: Number(req.pregunta.id)}
 		}).populate('subopcions').then(function(misOpciones){
 
-			var prueba = 
+			/*
+			misOpciones.forEach(function(name){}
+			*/
+			sails.log.verbose(Respuesta);
+			sails.log.verbose(misOpciones[0].subopcions[0].valor);
+			sails.log.verbose(misOpciones[0].id);
 
-			while (misOpciones.subopcions.length){
-				if( == miRespuesta) {
-					res.send('OK');
+			for (var i = 0; i < misOpciones.length; i++) { 
+				if (misOpciones[i].id == Respuesta){
+					Puntos ++;
+					sails.log.verbose('Correcto');
+					res.send(Puntos);
+				} else{
+					sails.log.verbose('Incorrecto');
+					res.send('NO');
+					next(new Error('Respuesta Incorrecta'));
 				}
-	    	}
+			}
 
-			next();
 		}).catch(function(error){next(error);});
 	},
 };
 
+ 
