ef553b88beb4f931a7b6495606ed6e37ac47cf4e
alexBaquerizo <elbatracio_21@hotmail.com>
2016-02-24 12:19:12 +0100

    integrando todas los tipos de pregunta

--- api/models/Pregunta.js
+++ api/models/Pregunta.js
@@ -49,12 +49,14 @@ module.exports = {
             switch(this.tipo){
                 case "truefalse":
 
-                    this.comprobar_truefalse(respuesta, function cb(puntuacion, texto){
+                    this.comprobar_truefalse(respuesta, function (puntuacion, texto){
                         Alumno.findOne({
                             where: {user: user}
                         }).then(function(alumno){
                             if(alumno){
-                                sails.log.verbose(texto);
+                                console.log('------------');
+                                sails.log.verbose(texto, puntuacion);
+                                console.log('------------');
                                 Respuesta.create({valor: texto, puntuacion: puntuacion, cuestionario: cuestionario, pregunta: pregunta, alumno: alumno.id})
                                 .exec(function createCB(err, created){
                                     res.json(created);
@@ -68,24 +70,106 @@ module.exports = {
 
                     break;
 
+                case "numerical":
+
+                    this.comprobar_numerical(respuesta, function (puntuacion, texto){
+                        Alumno.findOne({
+                            where: {user: user}
+                        }).then(function(alumno){
+                            if(alumno){
+                                console.log('------------');
+                                sails.log.verbose(texto, puntuacion);
+                                console.log('------------');
+                                Respuesta.create({valor: texto, puntuacion: puntuacion, cuestionario: cuestionario, pregunta: pregunta, alumno: alumno.id})
+                                .exec(function createCB(err, created){
+                                    res.json(created);
+                                })
+
+                            }else{
+                                sails.log.verbose("No estas autenticado como usuario Alumno");
+                            }
+                        })
+                    });
+                    
+                    break;
+
+                case "multichoice":
+
+                    this.comprobar_multichoice(respuesta, function (puntuacion, texto){
+                        Alumno.findOne({
+                            where: {user: user}
+                        }).then(function(alumno){
+                            if(alumno){
+                                console.log('------------');
+                                sails.log.verbose(texto, puntuacion);
+                                console.log('------------');
+                                Respuesta.create({valor: texto, puntuacion: puntuacion, cuestionario: cuestionario, pregunta: pregunta, alumno: alumno.id})
+                                .exec(function createCB(err, created){
+                                    res.json(created);
+                                })
+
+                            }else{
+                                sails.log.verbose("No estas autenticado como usuario Alumno");
+                            }
+                        })
+                    });
+
+                    break;
+
+                case "matching":
+
+                    this.comprobar_matching(respuesta, function (puntuacion, texto){
+                        Alumno.findOne({
+                            where: {user: user}
+                        }).then(function(alumno){
+                            if(alumno){
+                                console.log('------------');
+                                sails.log.verbose(texto, puntuacion);
+                                console.log('------------');
+                                Respuesta.create({valor: texto, puntuacion: puntuacion, cuestionario: cuestionario, pregunta: pregunta, alumno: alumno.id})
+                                .exec(function createCB(err, created){
+                                    res.json(created);
+                                })
+
+                            }else{
+                                sails.log.verbose("No estas autenticado como usuario Alumno");
+                            }
+                        })
+                    }); 
+                    
+                    break;
+
+                case "essay":
+
+                   this.comprobar_essay(respuesta, function (puntuacion, texto){
+                        Alumno.findOne({
+                            where: {user: user}
+                        }).then(function(alumno){
+                            if(alumno){
+                                console.log('------------');
+                                sails.log.verbose(texto, puntuacion);
+                                console.log('------------');
+                                Respuesta.create({valor: texto, puntuacion: puntuacion, cuestionario: cuestionario, pregunta: pregunta, alumno: alumno.id})
+                                .exec(function createCB(err, created){
+                                    res.json(created);
+                                })
+
+                            }else{
+                                sails.log.verbose("No estas autenticado como usuario Alumno");
+                            }
+                        })
+                    }); 
+                
+                break;
+
                 case "shortanswer":
                     
-                    break;
-                case "numerical":
-                    
-                    break;
-                case "multichoice":
-                    
-                    break;
-                case "matching":
-                    
-                    break;
-                case "essay":
-                
                 break;
             }
 
         },
+
+        /** true-false **/
 
         comprobar_truefalse: function(respuesta, cb){
 
@@ -100,16 +184,119 @@ module.exports = {
                     
                     sails.log.verbose(subopcion);
 
-                    if(subopcion.nombre === 'fraction'){
+                    if(subopcion.nombre === 'fraccion'){
                         puntuacion = subopcion.valor;
                     }
 
                     if(subopcion.nombre === 'text'){
                         texto = subopcion.valor;
                     }
-                    cb(puntuacion, texto);
+                
                 });
+
+                cb(puntuacion, texto);
             });
+        },
+
+        /** multichoice - luis **/
+
+        comprobar_multichoice: function(respuesta, cb){
+
+            Subopcion.findOne({
+                where: {opcion: Number(respuesta), nombre: "fraccion"}
+            }).then(function(subopcion){
+                var puntuacion = subopcion.valor;
+                Subopcion.findOne({
+                    where: {opcion: Number(respuesta), nombre: "text"}
+                }).then(function(subopcion){
+                    var texto = subopcion.valor;
+                    cb(puntuacion, texto);
+                })  
+            });
+        },
+
+        /** numerical - lenin y osmar **/
+
+        comprobar_numerical: function(respuesta, cb) {
+            var guardavalor1;
+            var guardavalor2;
+
+            Opcion.findOne({
+                where: { id: Number(respuestaRec) }
+            }).populate('subopciones').then(function(opcion){
+                    //console.log(opcion.subopciones); 
+                opcion.subopciones.forEach(function(subopcion){
+                    //De opcion entro a subopciones y con el forEach recorro en subopciones una subopcion
+                    if(subopcion.nombre=='fraccion'){
+                        guardavalor1=subopcion.valor; //guardo el valor de fraccion
+                        //sails.log.verbose(guardavalor1);
+                    }
+                    if(subopcion.nombre=='text'){
+                        guardavalor2=subopcion.valor; //guardo el valor de texto
+                        //sails.log.verbose(guardavalor2);
+                    }
+                })
+
+                cb(guardavalor1, guardavalor2);
+
+            })
+        },
+
+         /** essay - adrian **/
+
+        comprobar_essay: function(respuesta, cb) {
+        
+            var valor1;
+            var valor2;
+                
+            Opcion.findOne({
+                where: { id: Number(respuestaRecibida) }
+            }).populate('subopciones').then(function(opcion){
+                    
+                opcion.subopciones.forEach(function(subopcion){
+                    
+                    if(subopcion.nombre=='responserequired'){
+                        valor1=subopcion.valor; 
+                        
+                    }
+                    if(subopcion.nombre=='responsefieldlines'){
+                        valor2=subopcion.valor; 
+                        
+                    }
+
+                    cb(valor1, valor2);
+            });
+        },
+
+        /** matching - antonio **/
+
+        comprobar_matching: function(respuesta, cb) {
+
+            Incremento = 0;
+            Puntos = 0;
+
+            // Cliente envia ID de la 'subquestion' y el ID de la subopcion 'answer'.
+
+            Opcion.find().where({ pregunta: req.pregunta.id, tipoOpcion: 'subquestion' }).populate('subopciones')
+                .then(function(opciones){
+                        
+                    Puntos = 0;
+                    Incremento = Math.floor(100 / opciones.length);
+
+                    for ( i = 0 ; i < Answered.length ; i += 2 ) {
+                            for ( n = 0 ; n < opciones.length ; n++ ) {
+                                    if ( Answered[i] == opciones[n].subopciones[0].valor && 
+                                             Answered[i+1] == opciones[n].subopciones[1].valor ) {
+
+                                            Puntos += Incremento;
+                                    }
+                            }
+                    }
+
+                    cb(Puntos, respuesta);
+                        
+                }).catch(function(error){console.log(error);});
+                
         },
 
     }
